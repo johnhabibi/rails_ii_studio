34db2f3c03cf4d1bb05d71e83e677a7cc5ca71f5
John Habibi <johnhabibi@gmail.com>
2018-03-05 22:48:40 -0600

    create admin and refine authorizations further

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180228221638) do
+ActiveRecord::Schema.define(version: 20180306030450) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -27,12 +27,12 @@ ActiveRecord::Schema.define(version: 20180228221638) do
   end
 
   create_table "reviews", force: :cascade do |t|
-    t.string   "name"
     t.integer  "stars"
     t.text     "comment"
     t.integer  "movie_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
     t.index ["movie_id"], name: "index_reviews_on_movie_id"
   end
 
@@ -40,8 +40,9 @@ ActiveRecord::Schema.define(version: 20180228221638) do
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.boolean  "admin",           default: false
   end
 
 end
